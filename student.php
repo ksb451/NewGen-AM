0a9b717be987a6f9bef468ebc1cb9e65362684f0
ksb451 <iec2018043@iiita.ac.in>
2019-08-31 02:47:19 +0530

    style done

--- student.php
+++ student.php
@@ -9,12 +9,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Teacher login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <form action="backend/login_student.php" method="POST">
-        <input type="text" name="roll" placeholder="Roll no...">
-        <input type="password" name="password" placeholder="Password...">
-        <button type="submit" name="login">Log In</button>
+	<div class="logo">
+		<img  src="logo.svg" alt="Logo" width="50px">
+	</div>
+	<div class="name">
+		Sexy
+		Name
+	</div>
+	<div class="about">
+		Doo dood odood odood
+	</div>
+
+    <form class="form" action="backend/login_student.php" method="POST">
+    	
+        <input class="login" type="text" name="roll" placeholder="Login ID">
+  
+        <input class="pass" type="password" name="password" placeholder="Password">
+        <br>
+        <button class="lbutton" type="submit" name="login">Log In</button>
     </form>
+	
 </body>
 </html>
